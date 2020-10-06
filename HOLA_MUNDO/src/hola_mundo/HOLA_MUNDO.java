54713f88a458d18fce426651ff48b7cf87c9648c
jorgeelias437 <jorgeelias437@gmail.com>
2020-10-05 23:44:31 -0600

    se modifico el proyecto 2222

--- HOLA_MUNDO/src/hola_mundo/HOLA_MUNDO.java
+++ HOLA_MUNDO/src/hola_mundo/HOLA_MUNDO.java
@@ -17,6 +17,7 @@ public class HOLA_MUNDO {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola jorge");
+        System.out.println("holaaaaaa");
     }
     
 }
